7639fe3797c3b85361cc82d3e0e7999a577d5db1
Grantjos12 <grantjoslyn@gmail.com>
2020-11-30 21:10:53 -0600

    added inital Header.js & init packages

--- watercooler/src/App.js
+++ watercooler/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div> This working</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
