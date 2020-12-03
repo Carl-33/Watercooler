25851a8a4c1421cf21a2da58a2b3536001bdec69
Grantjos12 <grantjoslyn@gmail.com>
2020-12-03 13:10:31 -0600

    added favicon and dashboard draft

--- watercooler/client/src/components/Pages/Dashboard.js
+++ watercooler/client/src/components/Pages/Dashboard.js
@@ -1,7 +1,77 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-const Forum = () => {
-  return <div>This is the Dashboard Page</div>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: theme.spacing(0, 3),
+  },
+  paper: {
+    maxWidth: 500,
+    margin: `${theme.spacing(8)}px auto`,
+    padding: theme.spacing(2),
+  },
+}));
+
+const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={5}
+        >
+          <Grid item>
+            <Button variant="outlined" color="primary">
+              Forum
+            </Button>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <Typography>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Button variant="outlined" color="primary">
+              Amazon Forum
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Button variant="outlined" color="primary">
+              Other Forum
+            </Button>
+          </Grid>
+          <Grid item xs>
+            <Typography>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 };
 
-export default Forum;
+export default Dashboard;
