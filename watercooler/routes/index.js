8524d0fd72340284df732a3b38a2a045ee54ff50
Grantjos12 <grantjoslyn@gmail.com>
2020-12-03 11:24:30 -0600

    this should work

--- watercooler/routes/index.js
+++ watercooler/routes/index.js
@@ -1,13 +1,12 @@
 const path = require("path");
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-})
+// router.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-router.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
-
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 module.exports = router;
--- watercooler/server.js
+++ watercooler/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 
