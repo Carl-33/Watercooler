055cec8b835512b4a2000af079b9e450c6850e6c
Grantjos12 <grantjoslyn@gmail.com>
2021-01-28 14:27:34 -0600

    added build folder

---
+++ client/build/precache-manifest.f576a8975a52beaf81f77e32a8df2142.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558a0f81fd27373a23a0388189499413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a3e08df0a2c5a65f16",
+    "url": "/static/css/main.6289bdd4.chunk.css"
+  },
+  {
+    "revision": "fdef6602a31db3e7b6c8",
+    "url": "/static/js/2.5d6d9003.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.5d6d9003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a3e08df0a2c5a65f16",
+    "url": "/static/js/main.5b82aa79.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "502f8378d1b30758135ac83062f9fdeb",
+    "url": "/static/media/justcuppy.502f8378.png"
+  },
+  {
+    "revision": "6fd2d1d69b805365865ad7b1c37f008b",
+    "url": "/static/media/wally3.6fd2d1d6.png"
+  },
+  {
+    "revision": "f61ab255ee55c20d39e1009cc2861b33",
+    "url": "/static/media/wallyandcuppy.f61ab255.png"
+  },
+  {
+    "revision": "bb70223990536d6591858a2823429481",
+    "url": "/static/media/water-cooler-talk.bb702239.png"
+  }
+]);
